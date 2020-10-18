9608bb7879b5167f6882b06a1644441beef165a1
Glazelf <gideon.steenbergen@gmail.com>
2020-10-18 16:48:27 +0200

    start with an easier level, and add a sleep method because i am the boss here,, javascript cant tell me what to do

--- commands/inputs/1-2.js
+++ commands/inputs/1-2.js
@@ -5,14 +5,23 @@ exports.run = async (client, message) => {
         if (message.author.id !== client.config.ownerID) return message.reply(globalVars.lackPerms);
 
         const conn = require("../../switch/index");
+        const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
         let input = message.content.split(` `, 2);
         let count = input[1];
+        if(!count) count = 1;
         if (isNaN(count)) return message.channel.send(`> Provided variable isn't a number, ${message.author}.`);
         if (count < 1) return message.channel.send(`> Provided number needs to be above 0, ${message.author}.`);
 
+        // Input loop for 1-2
         while (count > 0) {
-            // Input loop for 1-1
-            console.log("Sardines!")
+            await sleep(500);
+            // Enter level
+            conn.write("click A \r\n");
+            await sleep(3000);
+            // Select Base Panel
+            conn.write("click A \r\n");
+            await sleep(3000)
+            console.log("slept")
             count -= 1;
         };
 
